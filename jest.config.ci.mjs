0e6c6ffb4e51c43afa145cf0caec0d1fd30b8c72
Lionel Besson <lionel.besson@gmail.com>
2022-09-13 17:30:44 +0200

    Do not use codacy

--- jest.config.ci.mjs
+++ jest.config.ci.mjs
@@ -2,7 +2,7 @@ import baseConfig from './jest.config.mjs';
 
 export default {
   ...baseConfig,
-  coverageDirectory: 'reports/coverage',
+  coverageDirectory: 'reports/coverage/unit',
   reporters: ['default', ['jest-junit', { outputName: 'reports/junit/js-test-results.xml' }]],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts']
--- jest.config.s3.ci.mjs
+++ jest.config.s3.ci.mjs
@@ -2,7 +2,7 @@ import baseConfig from './jest.config.s3.mjs';
 
 export default {
   ...baseConfig,
-  coverageDirectory: 'reports/coverage',
+  coverageDirectory: 'reports/coverage/s3',
   reporters: ['default', ['jest-junit', { outputName: 'reports/junit/js-test-results.xml' }]],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts']
