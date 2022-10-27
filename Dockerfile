58ffc0cfc938322a70a2af424b2bf781e52042dc
Bruno Besson <bruno.besson@gmail.com>
2022-10-27 18:16:07 +0200

    Containerized node best practices (#66)
    See https://snyk.io/blog/10-best-practices-to-containerize-nodejs-web-applications-with-docker/
    
    Co-authored-by: Lionel Besson <lionel.besson@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
 FROM node:lts-alpine AS build
+RUN apk add --no-cache dumb-init
+WORKDIR /usr
+COPY package.json package-lock.json tsconfig.json ./
+RUN npm ci --fund=false
+COPY src ./src
+RUN npm run build
+RUN npm prune --omit "dev"
 
+FROM node:lts-alpine
 RUN apk add --no-cache \
     librsvg \
     imagemagick
-
 RUN apk add --no-cache \
     terminus-font \
     ttf-inconsolata \
@@ -22,17 +29,15 @@ RUN apk add --no-cache \
     ttf-droid \
     ttf-droid-nonlatin \
     msttcorefonts-installer
-
-COPY --chown=node:node package.json package-lock.json tsconfig.json ./
-COPY --chown=node:node src ./src
-RUN npm install && npm run build
-
-# Clean dev packages
-RUN npm prune --omit "dev"
-
+COPY --from=build /usr/bin/dumb-init /usr/bin/dumb-init
+WORKDIR /usr/bin/app
+COPY --from=build --chown=node:node /usr/package.json ./
+COPY --from=build --chown=node:node /usr/node_modules ./
+COPY --from=build --chown=node:node /usr/dist ./
+ENV NODE_ENV production
 ENV DEBUG "c2c_images:*"
 ENV SERVICE_PORT 8080
 ENV METRICS_PORT 8081
 EXPOSE 8080 8081
-
-CMD ["npm", "start"]
+USER node
+CMD ["dumb-init", "node", "server.js"]
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,50 @@
+import { ErrorCallback, retry } from 'async';
+import type { Server } from 'node:http';
 import { DISABLE_PROMETHEUS_METRICS, METRICS_PATH, METRICS_PORT, SERVICE_PORT } from './config.js';
 import { imageMagickVersion, isAvifSupported, isWebpSupported, rsvgConvertVersion } from './image/convert.js';
 import { koa } from './koa/app.js';
 import { log } from './log.js';
 import { metricsServer } from './metrics/metrics.js';
+
+async function closeServer(server: Server): Promise<void> {
+  const checkPendingRequests = (callback: ErrorCallback<Error | undefined>): void => {
+    server.getConnections((err: Error | null, pendingRequests: number) => {
+      if (err) {
+        callback(err);
+      } else if (pendingRequests) {
+        callback(Error(`Number of pending requests: ${pendingRequests}`));
+      } else {
+        callback(undefined);
+      }
+    });
+  };
+
+  return new Promise<void>((resolve, reject) => {
+    retry({ times: 10, interval: 1000 }, checkPendingRequests, (error?: Error | null) => {
+      if (error) {
+        server.close(() => reject(error));
+      } else {
+        server.close(() => resolve());
+      }
+    });
+  });
+}
+
+async function closeGracefully(signal: string, ...servers: Server[]): Promise<void> {
+  log.info(`Received signal to terminate: ${signal}`);
+
+  koa.context['shuttingDown'] = true;
+
+  for (const s of servers.map(closeServer)) {
+    try {
+      await s;
+    } catch (e) {
+      log.error('Error in graceful shutdown ', e);
+    }
+  }
+
+  process.kill(process.pid, signal);
+}
 
 // check required tools exist
 log.info('Using rsvg-convert =>', rsvgConvertVersion());
@@ -11,7 +53,7 @@ log.info(`Webp write is ${isWebpSupported ? '' : 'un'}supported`);
 log.info(`Avif write is ${isAvifSupported ? '' : 'un'}supported`);
 
 // Listen for REST request
-koa.listen(SERVICE_PORT);
+const server = koa.listen(SERVICE_PORT);
 log.info(`Service starting on port ${SERVICE_PORT}`);
 
 // Export metrics for prometheus
@@ -19,3 +61,6 @@ if (!DISABLE_PROMETHEUS_METRICS) {
   metricsServer.listen(METRICS_PORT);
   log.info(`Prometheus metrics can be scrapped at port ${METRICS_PORT} path ${METRICS_PATH}`);
 }
+
+process.once('SIGINT', async (signal: string) => closeGracefully(signal, server, metricsServer));
+process.once('SIGTERM', async (signal: string) => closeGracefully(signal, server, metricsServer));
