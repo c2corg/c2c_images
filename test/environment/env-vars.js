35fe8c94276ce9f175cd3789dfd30b82b7a76f78
Lionel Besson <lionel.besson@gmail.com>
2022-10-28 15:25:19 +0200

    Make temp folder optional (#87)

--- test/environment/env-vars.js
+++ test/environment/env-vars.js
@@ -3,8 +3,7 @@ import os from 'node:os';
 
 process.env.NODE_ENV = 'development';
 process.env.STORAGE_BACKEND = 'local';
-process.env.TEMP_FOLDER = `${os.tmpdir}/temp`;
-process.env.INCOMING_FOLDER = `${os.tmpdir}/incoming`;
-process.env.ACTIVE_FOLDER = `${os.tmpdir}/active`;
+process.env.INCOMING_FOLDER = `${os.tmpdir()}/images/incoming`;
+process.env.ACTIVE_FOLDER = `${os.tmpdir()}/images/active`;
 process.env.API_SECRET_KEY = 'my secret';
 process.env.AUTO_ORIENT_ORIGINAL = '1';
