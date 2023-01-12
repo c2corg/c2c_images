bbaabc456688834c1b62ce4721a299feb4c90ec0
Lionel Besson <lionel.besson@gmail.com>
2023-01-12 10:55:45 +0100

    Code cleaning

--- src/koa/apionly.ts
+++ src/koa/apionly.ts
@@ -6,11 +6,15 @@ import { ApiSecret } from './validation.js';
  */
 export const apiOnly: Koa.Middleware = async (ctx, next) => {
   const result = ApiSecret.safeParse(ctx.request.body);
-  if (!result.success) {
-    if (result.error.issues[0]?.message === 'Required') {
-      ctx.throw(401, 'Missing secret key.');
-    }
-    ctx.throw(403, 'Bad secret key.');
+
+  if (result.success) {
+    await next();
+    return;
   }
-  await next();
+
+  if (result.error.issues[0]?.message === 'Required') {
+    ctx.throw(401, 'Missing secret key.');
+  }
+
+  ctx.throw(403, 'Bad secret key.');
 };
